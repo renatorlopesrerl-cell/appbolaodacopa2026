bf619caa0dae68c2cbdb696a35f1bccebcec73c9
Renato Lopes <renatorlopes.rerl@gmail.com>
2026-02-23 20:57:26 -0300

    feat: Initialize Capacitor configuration and add IDE project files.

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.palpiteirodacopa',
+  appName: 'Palpiteiro da Copa 2026',
+  webDir: 'dist'
+};
+
+export default config;
